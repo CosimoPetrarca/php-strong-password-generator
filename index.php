1bbdd5273c6fb659ce4cc64fe60f7793e0a49417
P4anix <pancok_1@yahoo.it>
2023-04-11 18:14:31 +0200

    bootstrap

--- index.php
+++ index.php
@@ -9,18 +9,27 @@ include __DIR__ . './function.php'
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Strong Password Generator</title>
+    <!-- cdn bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Strong Password Generator</h1>
+    <div class="container">
+        <h1 class="my-5">Strong Password Generator</h1>
 
-	<form method="GET" action="">
-		<label for="lunghezza_password">Lunghezza password:</label>
-		<input type="number" id="lunghezza_password" name="lunghezza_password" placeholder="Inserisci il numero di caratteri">
-		<input type="submit" value="Genera password">
-	</form>
+        <form method="GET" action="">
+            <div class="mb-3">
+                <label for="lunghezza_password" class="form-label">Lunghezza password:</label>
+                <input type="number" id="lunghezza_password" name="lunghezza_password" class="form-control" placeholder="Inserisci il numero di caratteri">
+            </div>
+            <button type="submit" class="btn btn-primary">Genera password</button>
+        </form>
 
-    <?php if (!empty($_GET['lunghezza_password'])) : ?>
-            <p><?php echo generatorePassword($lunghezza); ?></p>
-     <?php endif; ?>
+        <?php if (!empty($_GET['lunghezza_password'])) : ?>
+            <h5 class="my-4">La password generata è: <span class="text-success"><?php echo generatorePassword($lunghezza); ?></span></h5>
+        <?php endif; ?>
+    </div>
+
+     <!-- js bootstrap -->
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
